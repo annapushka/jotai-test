e92e86f6d53005322d6c387453d211d0b385630f
anna <anna.pushkareva@smddev.com>
2023-02-14 12:41:54 +0300

    jotai test counter added

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { atom, useAtom } from "jotai";
+import "./App.css";
+
+const countAtom1 = atom(0);
+const countAtom2 = atom(0);
+
+const Counter1 = () => {
+  const [count, setCount] = useAtom(countAtom1);
+  const handleClick = () => setCount((c) => c + 1);
+
+  return (
+    <div>
+      {count}
+      <button onClick={handleClick}>+</button>
+    </div>
+  );
+};
+
+const Counter2 = () => {
+  const [count, setCount] = useAtom(countAtom2);
+  const handleClick = () => setCount((c) => c + 1);
+
+  return (
+    <div>
+      {count}
+      <button onClick={handleClick}>+</button>
+    </div>
+  );
+};
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Counter1 />
+      <Counter2 />
+    </div>
+  );
+};
